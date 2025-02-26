ab22a3b3836ace4d4a861cc7d456ad304d18a307
Marc Frances <marc.frances.t@gmail.com>
2025-02-27 00:31:07 +0100

    Add versioning

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
+app.get('/version', (req, res) => {
+  // change this string to ensure a new version deployed
+  res.send('1')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
