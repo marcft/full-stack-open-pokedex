fab8d2bccf338eb5622d808ee0adff65ad9101b5
Marc Frances <marc.frances.t@gmail.com>
2025-02-27 18:16:13 +0100

    Upgrade version

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
   // change this string to ensure a new version deployed
-  res.send('5')
+  res.send('6')
 })
 
 app.get('/health', (req, res) => {
