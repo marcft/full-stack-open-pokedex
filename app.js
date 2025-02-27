ee6c1099229a7efc6141e631cd16fb804bb2a15a
Marc Frances <marc.frances.t@gmail.com>
2025-02-27 09:15:19 +0100

    Version 2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
   // change this string to ensure a new version deployed
-  res.send('1')
+  res.send('2')
 })
 
 app.listen(PORT, () => {
