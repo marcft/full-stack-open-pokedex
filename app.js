fe479433f726e5f16dfb5166e6f43a004f7e981e
Marc Frances <marc.frances.t@gmail.com>
2025-03-03 01:54:40 +0100

    Fix app.js error

--- app.js
+++ app.js
@@ -15,8 +15,6 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
-throw new Error('This is a test error')
-
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
