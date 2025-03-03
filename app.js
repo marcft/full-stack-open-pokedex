4ebc2da118953bfe82a1e7ae224c6fb7cb711ce4
Marc Frances <marc.frances.t@gmail.com>
2025-03-03 14:08:11 +0100

    Trigger redeployment

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
   // change this string to ensure a new version deployed
-  res.send('0.1.7')
+  res.send('0.1.8')
 })
 
 app.get('/health', (req, res) => {
