3b9fc229c302286e0d15a6df7b4f83355773edb3
Marc Frances <marc.frances.t@gmail.com>
2025-03-03 12:44:24 +0100

    Fix health check so now it throws error

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
   // change this string to ensure a new version deployed
-  throw new Error('new version deployed')
   res.send('0.1.7')
 })
 
